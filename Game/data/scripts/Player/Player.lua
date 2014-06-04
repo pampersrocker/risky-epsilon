414066542e4db449fa49ffbc449ec4c2ee211395
ruben <rubenmueller90@gmail.com>
2014-06-04 13:26:08 +0200

    Merge branch 'develop' of https://github.com/pampersrocker/risky-epsilon into develop
    Conflicts:
    	Game/data/scripts/Player/Player.lua

--- Game/data/scripts/Player/Player.lua
+++ Game/data/scripts/Player/Player.lua
@@ -11,9 +11,9 @@ function PlayerMeta:initializeGameObject( )
 	cinfo.restitution = 0.0
 	cinfo.friction = 10.0
 	cinfo.maxLinearVelocity = 300.0
-	cinfo.maxAngularVelocity = 200.0
+	cinfo.maxAngularVelocity = 100.0
 	--cinfo.linearDamping = 1.0
-	cinfo.angularDamping = 1.0
+	cinfo.angularDamping = 10.0
 	cinfo.position = Vec3(0.0, 0.0, 20.0)
 	CreatePhysicsComponent( self , cinfo )
 	CreateRenderComponent(self, "data/models/Sphere/sphere.thmodel")
@@ -37,8 +37,8 @@ function PlayerMeta.update( guid, elapsedTime )
 	
 
 	if InputHandler:gamepad(0):isConnected() then
-		local leftTorque = player.currentAngularVelocityLeft:mulScalar(-InputHandler:gamepad(0):leftStick().x)
-		local rightTorque =player.currentAngularVelocityForward:mulScalar(InputHandler:gamepad(0):leftStick().y)
+		local leftTorque = viewDir:mulScalar(1000):mulScalar(-InputHandler:gamepad(0):leftStick().x)
+		local rightTorque = rightDir:mulScalar(1000):mulScalar(InputHandler:gamepad(0):leftStick().y)
 		player.go.rb:applyTorque(elapsedTime, leftTorque + rightTorque)
 	else
 		if (InputHandler:isPressed(Key.A)) then
@@ -52,7 +52,7 @@ function PlayerMeta.update( guid, elapsedTime )
 		elseif (InputHandler:isPressed(Key.W)) then
 			--player.pc.rb:applyLinearImpulse(rightDir:mulScalar(moveSpeed))
 			player.go.angularVelocitySwapped = false
-			player.go.rb:applyTorque(elapsedTime,player.currentAngularVelocityForward)
+			player.go.rb:applyTorque(elapsedTime,-rightDir:mulScalar(1000))
 		elseif (InputHandler:isPressed(Key.S)) then
 			--player.pc.rb:applyLinearImpulse(rightDir:mulScalar(moveSpeed))
 			player.go.angularVelocitySwapped = false
